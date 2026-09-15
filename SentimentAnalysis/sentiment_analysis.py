25b869e661350c0b814592d9575237f69f1d7f0f
kalyanrockzz04 <kalyanrockzz04@gmail.com>
2026-09-15 03:23:21 -0400

    Complete sentiment analysis final project

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,98 @@
+import requests
+
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+
+    myobj = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    header = {
+        "grpc-metadata-mm-model-id":
+        "sentiment_aggregated-bert-workflow_lang_multi_stock"
+    }
+
+    response = requests.post(
+        url,
+        json=myobj,
+        headers=header
+    )
+
+    return response.text
+import requests
+import json
+
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+
+    myobj = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    header = {
+        "grpc-metadata-mm-model-id":
+        "sentiment_aggregated-bert-workflow_lang_multi_stock"
+    }
+
+    response = requests.post(
+        url,
+        json=myobj,
+        headers=header
+    )
+
+    formatted_response = json.loads(response.text)
+
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+
+    return {
+        'label': label,
+        'score': score
+    }
+import requests
+import json
+
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+
+    myobj = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    header = {
+        "grpc-metadata-mm-model-id":
+        "sentiment_aggregated-bert-workflow_lang_multi_stock"
+    }
+
+    response = requests.post(
+        url,
+        json=myobj,
+        headers=header
+    )
+
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+
+    elif response.status_code == 500:
+        label = None
+        score = None
+
+    else:
+        label = None
+        score = None
+
+    return {
+        'label': label,
+        'score': score
+    }
